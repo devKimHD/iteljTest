757ba14bf8e3e6c471baa213fcc68d07317b3833
devKimHD <khd30425@gmail.com>
2022-08-26 17:28:21 +0900

    4번째 커밋

--- javaintel/src/solvice/khd/NoIntelPull.java
+++ javaintel/src/solvice/khd/NoIntelPull.java
@@ -2,6 +2,7 @@ package solvice.khd;
 
 public class NoIntelPull {
     public static void main(String[] args) {
-        System.out.println("리셋 하드 전");
+        System.out.println("리셋 ??");
+        System.out.println("낙서중");
     }
 }

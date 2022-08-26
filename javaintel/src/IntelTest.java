11c1f4b75987bf7e00970eb9cfd8c36b4c53e361
devKimHD <105334841+devKimHD@users.noreply.github.com>
2022-08-26 17:34:04 +0900

    Update IntelTest.java
    로컬내 워크 스페이스 친구가 자동으로 백업이 되는지 보자

--- javaintel/src/IntelTest.java
+++ javaintel/src/IntelTest.java
@@ -1,5 +1,5 @@
 public class IntelTest {
     public static void main(String[] args) {
-        System.out.println("이거 두번째커밋인척 하는 메인에서 수정하는 풀테스트");
+        System.out.println("이거 두번째커밋인척 하는 메인에서 수정하는 풀테스트 시점을 마저 테스트해보자");
     }
 }

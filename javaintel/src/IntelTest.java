1e11ef21fdb8985a2d838c872e499bec2f68d90e
devKimHD <khd30425@gmail.com>
2022-08-26 17:04:18 +0900

    최초커밋

---
+++ javaintel/src/IntelTest.java
@@ -0,0 +1,5 @@
+public class IntelTest {
+    public static void main(String[] args) {
+        System.out.println("no package");
+    }
+}
---
+++ javaintel/src/solvice/khd/NoIntelPull.java
@@ -0,0 +1,7 @@
+package solvice.khd;
+
+public class NoIntelPull {
+    public static void main(String[] args) {
+        System.out.println("이건 패키지있다");
+    }
+}
